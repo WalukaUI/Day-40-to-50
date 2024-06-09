e065ae18c12fcc3d59c5086ea9e148fd520dc23e
walukaui <cwaluka@yahoo.com>
2024-06-09 17:11:10 -0500

    "day45 get tags"

--- Day45_beautySoup/main.py
+++ Day45_beautySoup/main.py
@@ -1,7 +1,34 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", encoding='utf-8')as file:
-    content = file.read()
+res =requests.get("https://news.ycombinator.com/news")
+web_page = res.text
 
-soup = BeautifulSoup(content, 'html.parser')
-print(soup.title.string)
+soup = BeautifulSoup(web_page, "html.parser")
+span = soup.select_one(".titleline")
+print(span.select_one("a").text)
+print(span.select_one("a").get("href"))
+
+#
+# with open("website.html", encoding='utf-8')as file:
+#     content = file.read()
+#
+# soup = BeautifulSoup(content, 'html.parser')
+# tags = soup.find_all(name="a")
+# for tag in tags:
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# classes= soup.find(name="h3", class_="heading")
+# print(classes)
+#
+# company_url = soup.select_one(selector="p a")
+# print(company_url.get("href"))
+#
+# name = soup.select_one(selector="#name")
+# print(name)
+#
+# all_classes = soup.select(selector=".heading")
+# print(all_classes)
