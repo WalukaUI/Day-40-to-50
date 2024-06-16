500641d2625ec2f5026e033ab1a034a9cd9a303c
walukaui <cwaluka@yahoo.com>
2024-06-16 15:43:46 -0500

    "get ele by classs and xpath in selenium"

--- Day48 Advanced web scrape/main.py
+++ Day48 Advanced web scrape/main.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
-
-
-#keep browser open after program finishes
+from selenium.webdriver.common.by import By
+# keep browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome()
-driver.get("https://www.amazon.com/")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com/dp/B08NB1VGBK/ref=sbl_dpx_kitchen-electric-cookware_B08WCLJ7JG_0?th=1")
+dollar_price = driver.find_element(By.CLASS_NAME, value="a-span12")
+print(dollar_price.text)
+print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
+get_atag_withoutclass = driver.find_element(By.XPATH, value='//*[@id="customerReviewsAttribute_feature_div"]/div/div[2]/div[1]/div/div/div[1]/div/span')
+print(get_atag_withoutclass.text)
+# close only tab
+driver.close()
+
+# close chrome
+# driver.quit()
