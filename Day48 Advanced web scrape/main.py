14df2b9c8460f2b8ea6b5f52a50695229d964a5a
walukaui <cwaluka@yahoo.com>
2024-06-16 17:23:01 -0500

    "create list with sel"

--- Day48 Advanced web scrape/main.py
+++ Day48 Advanced web scrape/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 # keep browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -8,9 +9,11 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.amazon.com/dp/B08NB1VGBK/ref=sbl_dpx_kitchen-electric-cookware_B08WCLJ7JG_0?th=1")
 dollar_price = driver.find_element(By.CLASS_NAME, value="a-span12")
 print(dollar_price.text)
-print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
-get_atag_withoutclass = driver.find_element(By.XPATH, value='//*[@id="customerReviewsAttribute_feature_div"]/div/div[2]/div[1]/div/div/div[1]/div/span')
+get_atag_withoutclass = driver.find_element(By.XPATH,
+                                            value='//*[@id="productSupportAndReturnPolicy-return-policy-anchor-text"]')
 print(get_atag_withoutclass.text)
+
+
 # close only tab
 driver.close()
 
---
+++ Day48 Advanced web scrape/test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# keep browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.python.org/")
+get_atag_withoutclass = driver.find_elements(By.XPATH,
+                                            value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li')
+
+new_lst = {}
+
+ev_time = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+ev_name = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+for x in range(len(ev_time)):
+    new_lst[x] = {"time": ev_time[x].text, "name": ev_name[x].text}
+
+print(new_lst)
+#-------------TEST------------------
+
+
+
