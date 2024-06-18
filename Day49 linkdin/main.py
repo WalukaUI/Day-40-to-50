edd3ef0d2cd7f752b5b58fda9bb8145596534ed2
walukaui <cwaluka@yahoo.com>
2024-06-18 10:52:03 -0500

    "Day49"

--- Day49 linkdin/main.py
+++ Day49 linkdin/main.py
@@ -1,43 +1,96 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+import time
+
+ACCOUNT_EMAIL = YOUR LOGIN EMAIL
+ACCOUNT_PASSWORD = YOUR LOGIN PASSWORD
+PHONE = YOUR PHONE NUMBER
 
 
-# # keep browser open after program finishes
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach", True)
-#
-# driver = webdriver.Chrome(options=chrome_options)
-# driver.get("https://orteil.dashnet.org/cookieclicker/")
-#
-# time.sleep(3)
-# driver.find_element(By.ID, value="langSelect-EN").click()
-#
-# time.sleep(3)
-# num = 1000
-#
-# driver.find_element(By.ID, value="bigCookie").click()
-# get_marks = driver.find_element(By.ID, value="cookies").text.split(" ")[0]
-# marks = int(get_marks)
-#
-# productids = []
-# prices = []
-#
-# while marks < num:
-#     driver.find_element(By.ID, value="bigCookie").click()
-#     marks = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
-#     if marks % 100 == 0:
-#         items = driver.find_elements(by=By.CSS_SELECTOR, value="#store #products .unlocked .content .price")
-#         if len(items) > 0:
-#             for x in items:
-#                 productids.append(x.get_attribute("id"))
-#                 prices.append(int(x.text))
-#         num = prices.index(min(prices))
-#         min_price = min(prices)
-#         while marks > min_price:
-#             driver.find_element(By.ID, value="bigCookie").click()
-#             time.sleep(0.1)
-#             driver.find_element(By.ID, value=f"product{num}").click()
-#             marks = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
-#         productids = []
-#         prices = []
+def abort_application():
+    # Click Close Button
+    close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+    close_button.click()
+
+    time.sleep(2)
+    # Click Discard Button
+    discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+    discard_button.click()
+
+
+# Optional - Keep the browser open if the script crashes.
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3586148395&f_LF=f_AL&geoId=101356765&"
+           "keywords=python&location=London%2C%20England%2C%20United%20Kingdom&refresh=true")
+
+# Click Reject Cookies Button
+time.sleep(2)
+reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
+reject_button.click()
+
+# Click Sign in Button
+time.sleep(2)
+sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
+sign_in_button.click()
+
+# Sign in
+time.sleep(5)
+email_field = driver.find_element(by=By.ID, value="username")
+email_field.send_keys(ACCOUNT_EMAIL)
+password_field = driver.find_element(by=By.ID, value="password")
+password_field.send_keys(ACCOUNT_PASSWORD)
+password_field.send_keys(Keys.ENTER)
+
+# CAPTCHA - Solve Puzzle Manually
+input("Press Enter when you have solved the Captcha")
+
+# Get Listings
+time.sleep(5)
+all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+
+# Apply for Jobs
+for listing in all_listings:
+    print("Opening Listing")
+    listing.click()
+    time.sleep(2)
+    try:
+        # Click Apply Button
+        apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+        apply_button.click()
+
+        # Insert Phone Number
+        # Find an <input> element where the id contains phoneNumber
+        time.sleep(5)
+        phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        # Check the Submit Button
+        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            abort_application()
+            print("Complex application, skipped.")
+            continue
+        else:
+            # Click Submit Button
+            print("Submitting job application")
+            submit_button.click()
+
+        time.sleep(2)
+        # Click Close Button
+        close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+        close_button.click()
+
+    except NoSuchElementException:
+        abort_application()
+        print("No application button, skipped.")
+        continue
+
+time.sleep(5)
+driver.quit()
